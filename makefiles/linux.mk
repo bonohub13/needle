6e860a446812ba7d87d1380c2e7c3fc422d61c6d
Kensuke Saito <ken.saito.0813@gmail.com>
2025-08-17 22:18:43 +0900

    Squashed commit of the following:
    commit e41f6239d94ae21771370862816e5cc673a725a8
    Author: Kensuke Saito <ken.saito.0813@gmail.com>
    Date:   Sun Aug 17 22:18:20 2025 +0900
    
        Added debug build options

--- makefiles/linux.mk
+++ makefiles/linux.mk
@@ -14,8 +14,15 @@ linux-clippy:
 pkg-linux:
 	$(CARGO) build --release --target=${TARGET_LINUX}
 
+debug-linux:
+	$(CARGO) build --target=${TARGET_LINUX}
+
 linux-clippy_docker:
 	@IMAGE_NAME=${RUST_DOCKER_IMAGE_NAME} TAG=${LINUX_IMAGE_TAG} CMD="make linux-clippy" \
+		make docker-exec
+
+debug-linux_docker:
+	@IMAGE_NAME=${RUST_DOCKER_IMAGE_NAME} TAG=${LINUX_IMAGE_TAG} CMD="make debug-linux" \
 		make docker-exec
 
 pkg-linux_docker:
