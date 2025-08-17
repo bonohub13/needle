e41f6239d94ae21771370862816e5cc673a725a8
Kensuke Saito <ken.saito.0813@gmail.com>
2025-08-17 22:18:20 +0900

    Added debug build options

--- makefiles/windows.mk
+++ makefiles/windows.mk
@@ -12,10 +12,17 @@ windows-clippy:
 pkg-windows:
 	$(CARGO) xwin build --release --target=${TARGET_WINDOWS}
 
+debug-windows:
+	$(CARGO) xwin build --target=${TARGET_WINDOWS}
+
 windows-clippy_docker:
 	@IMAGE_NAME=${RUST_DOCKER_IMAGE_NAME} TAG=${WINDOWS_IMAGE_TAG} CMD="make windows-clippy" \
 		make docker-exec
 
 pkg-windows_docker:
 	@IMAGE_NAME=${RUST_DOCKER_IMAGE_NAME} TAG=${WINDOWS_IMAGE_TAG} CMD="make pkg-windows" \
+		make docker-exec
+
+debug-windows_docker:
+	@IMAGE_NAME=${RUST_DOCKER_IMAGE_NAME} TAG=${WINDOWS_IMAGE_TAG} CMD="make debug-windows" \
 		make docker-exec
